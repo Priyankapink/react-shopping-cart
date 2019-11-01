86c0b9e93c263c2d0b1302d9d62c8e26b6a4c087
Priyanka Prakash <priyankaprakash@wimoku.com>
2019-11-01 23:36:02 +0530

    Shopping cart demo

---
+++ src/components/Header.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import Counter from "./Counter";
+import { findDOMNode } from "react-dom";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: this.props.cartItems,
+      mobileSearch: false
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+  handleMobileSearch(e) {
+    e.preventDefault();
+    this.setState({
+      mobileSearch: true
+    });
+  }
+  handleSearchNav(e) {
+    e.preventDefault();
+    this.setState(
+      {
+        mobileSearch: false
+      },
+      function() {
+        this.refs.searchBox.value = "";
+      }
+    );
+  }
+  handleClickOutside(event) {
+    const cartNode = findDOMNode(this.refs.cartPreview);
+    const buttonNode = findDOMNode(this.refs.cartButton);
+    if (cartNode.classList.contains("active")) {
+      if (!cartNode || !cartNode.contains(event.target)) {
+        event.stopPropagation();
+      }
+    }
+  }
+  componentDidMount() {
+    document.addEventListener(
+      "click",
+      this.handleClickOutside.bind(this),
+      true
+    );
+  }
+  componentWillUnmount() {
+    document.removeEventListener(
+      "click",
+      this.handleClickOutside.bind(this),
+      true
+    );
+  }
+  render() {
+    let cartItems;
+    cartItems = this.state.cart.map(product => {
+      return (
+        <li className="cart-item" key={product.name}>
+          <img className="product-image" src={product.image} />
+          <div className="product-info">
+            <p className="product-name">{product.name}</p>
+            <p className="product-price">{product.price}</p>
+          </div>
+          <div className="product-total">
+            <p className="quantity">
+              {product.quantity} {product.quantity > 1 ? "Nos." : "No."}{" "}
+            </p>
+            <p className="amount">{product.quantity * product.price}</p>
+          </div>
+          <a
+            className="product-remove"
+            href="#"
+            onClick={this.props.removeProduct.bind(this, product.id)}
+          >
+          </a>
+        </li>
+      );
+    });
+  
+    return (
+      <header>
+        <div className="container">
+          <h1>Shopping Cart</h1>
+          <div className="cart">
+            <div className="cart-info">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>No. of items</td>
+                    <td>:</td>
+                    <td>
+                      <strong>{this.props.totalItems}</strong>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Sub Total</td>
+                    <td>:</td>
+                    <td>
+                      <strong>{this.props.total}</strong>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
